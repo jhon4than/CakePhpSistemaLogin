ff313bcb581aa1e40018ce804a173399acf704d0
Jhonathan Meireles <jhon4than_jf@hotmail.com>
2022-04-14 11:41:54 -0300

    Atualização sistema

--- src/Application.php
+++ src/Application.php
@@ -54,15 +54,7 @@ class Application extends BaseApplication
                 (new TableLocator())->allowFallbackClass(false)
             );
         }
-
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
-        if (Configure::read('debug')) {
-            $this->addPlugin('DebugKit');
-        }
-
+        
         // Load more plugins here
     }
 
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -41,8 +41,6 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
             <?php if ($username): ?>
             <a <?= $this->Html->link(__('Logout'), ['controller' => 'Users' , 'action' => 'logout']) ?> </a>
             <?php endif; ?>
